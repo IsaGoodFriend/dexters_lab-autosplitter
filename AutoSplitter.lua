7a34117b7af3b936714047120baa9fc9f09deea6
Isabel S <42853091+IsaGoodFriend@users.noreply.github.com>
2019-07-18 19:56:23 -0600

    Update AutoSplitter.lua

--- AutoSplitter.lua
+++ AutoSplitter.lua
@@ -1,13 +1,14 @@
 -- based off code from Trysdyn Black, 2016 https://github.com/trysdyn/bizhawk-speedrun-lua
--- Made by IsaGoodFriend and Blazephlozard
+-- Made by IsaGoodFriend and Blazephlozard#8042
 
 local function init_livesplit()
     pipe_handle = io.open("//./pipe/LiveSplit", 'a')
 
     if not pipe_handle then
         error("\nFailed to open LiveSplit named pipe!\n" ..
-              "Please make sure LiveSplit is running and is at least 1.7, " ..
-              "then load this script again")
+              "Please make sure LiveSplit is running and is at least 1.7," ..
+              "then load this script again.  If you're using Bizhawk, make sure" ..
+			  "you're using LuaInterface core.")
     end
 
     pipe_handle:write("reset\r\n")
@@ -57,9 +58,9 @@ frametimer = memory.readbyte(0x032590)
 ----	0x40: Soldering Iron
 ----	0x80: Hammer
 -- 0xFFFF are dummy values to prevent index overflow
-deedeeSplits = 	{0000, 0016, 0020, 0028, 0028, 0038, 0054, 0069, 0079, 0092, 0104, 0108, 0112, 0124, 0xFFFF}
-machineSplits = {0000, 0005, 0006, 0009, 0009, 0011, 0012, 0016, 0021, 0023, 0028, 0030, 0041, 0043, 0xFFFF}
-toolSplits = 	{0x08, 0x08, 0x18, 0x18, 0x38, 0xB8, 0xB8, 0xF8, 0xF8, 0xF8, 0xF9, 0xF9, 0xF9, 0xF9, 0xFFFF}
+deedeeSplits = 	{0016, 0028, 0038, 0054, 0069, 0079, 0092, 0104, 0112, 0124, 0xFFFF}
+machineSplits = {0005, 0009, 0011, 0012, 0016, 0021, 0023, 0028, 0041, 0043, 0xFFFF}
+toolSplits = 	{0x08, 0x18, 0xB8, 0xB8, 0xF8, 0xF8, 0xF8, 0xF9, 0xF9, 0xF9, 0xFFFF}
 
 -- 030D0C On Screen Cursor
 -- Level design? 027E20
@@ -107,7 +108,6 @@ while true do
 			machineFlags2 = machineFlags2 / 2
 		end
 		
-		--console.log(machineCount)
 		if (deedeeCount >= deedeeSplits[split + 1] and tools >= toolSplits[split + 1] and machineCount >= machineSplits[split + 1]) then
 			send_time()
 			split = split + 1
@@ -119,13 +119,11 @@ while true do
 	end
 	
 	if (paused and newframetimer ~= frametimer and demovar ~= 0) then
-		--console.log("unpausing")
 		paused = false
 		pipe_handle:write("unpausegametime\r\n")
         pipe_handle:flush()
 		send_time()
 	elseif (not paused and newframetimer == frametimer) then 
-		--console.log("pausing")
 		paused = true
 		--using alwayspausegametime so that game time doesn't start early when starting a run
 		pipe_handle:write("alwayspausegametime\r\n")
