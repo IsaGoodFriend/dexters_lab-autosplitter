84bc7e0bcec735d0a8dc4b5e7d1fd9971ff479f8
Isabel S <42853091+IsaGoodFriend@users.noreply.github.com>
2019-07-22 18:51:01 -0600

    New Version
    Fixed a few bugs, and added support for sub splitting (which is the default)

--- AutoSplitter.lua
+++ AutoSplitter.lua
@@ -44,6 +44,8 @@ end
 pipe_handle = init_livesplit()
 memory.usememorydomain("Combined WRAM")
 
+useSubSplits = true
+
 paused = true
 mainmenucheck = false 
 split = 0
@@ -62,6 +64,10 @@ deedeeSplits = 	{0016, 0028, 0038, 0054, 0069, 0079, 0092, 0104, 0112, 0124, 0xF
 machineSplits = {0005, 0009, 0011, 0012, 0016, 0021, 0023, 0028, 0041, 0043, 0xFFFF}
 toolSplits = 	{0x08, 0x18, 0xB8, 0xB8, 0xF8, 0xF8, 0xF8, 0xF9, 0xF9, 0xF9, 0xFFFF}
 
+deedeeSplitsSub = 	{0000, 0004, 0008, 0012, 0016, 0020, 0024, 0028, 0028, 0032, 0036, 0038, 0038, 0042, 0046, 0050, 0054, 0058, 0062, 0065, 0069, 0073, 0075, 0079, 0083, 0084, 0088, 0092, 0096, 0100, 0104, 0108, 0108, 0108, 0108, 0109, 0109, 0109, 0112, 0116, 0120, 0120, 0124, 0xFFFF}
+machineSplitsSub =	{0000, 0001, 0003, 0003, 0005, 0006, 0008, 0009, 0009, 0010, 0011, 0011, 0011, 0011, 0012, 0012, 0012, 0014, 0016, 0016, 0016, 0018, 0019, 0021, 0021, 0021, 0023, 0023, 0024, 0026, 0028, 0030, 0031, 0032, 0033, 0035, 0037, 0039, 0041, 0042, 0043, 0043, 0043, 0xFFFF}
+toolSplitsSub = 	{0x08, 0x08, 0x08, 0x08, 0x18, 0x18, 0x18, 0x18, 0x38, 0x38, 0x38, 0x38, 0xB8, 0xB8, 0xB8, 0xB8, 0xB8, 0xF8, 0xF8, 0xF8, 0xF8, 0xF8, 0xF8, 0xF8, 0xF8, 0xF8, 0xF8, 0xF8, 0xF9, 0xF9, 0xF9, 0xF9, 0xF9, 0xF9, 0xF9, 0xF9, 0xF9, 0xF9, 0xF9, 0xF9, 0xF9, 0xF9, 0xF9, 0xFFFF}
+
 -- 030D0C On Screen Cursor
 -- Level design? 027E20
 -- 00E22A08 082ae200
@@ -72,6 +78,7 @@ while true do
 	newframetimer = memory.readbyte(0x032590)
 	pausevar = memory.readbyte(0x047C3E)
 	demovar = memory.readbyte(0x032584)
+	loading = memory.read_u32_le(0x0326A8)
 	
 	--Only send the game time every 256 frames, to reduce chance of lag
 	if (newframetimer ~= frametimer and newframetimer == 0) then
@@ -79,16 +86,19 @@ while true do
 	end
 	
 	--If game is at initial value, then restart splits
-	if (deedeeCount == 0 and machineCount == 0 and pausevar == 1) then
-		split = 0
-	end
 	
-	if (pausevar == 0x9F and not mainmenucheck) then
-		
+	if ((pausevar == 0x9F or (loading ~= 65535 and useSubSplits)) and not mainmenucheck) then
 		mainmenucheck = true
 		
 		tools = memory.readbyte(0x0325B8) + memory.readbyte(0x0325B9)
 		deedeeCount = memory.readbyte(0x032610);
+		
+		if (deedeeCount == 0 and machineCount == 0 and tools == 0 and pausevar == 1) then
+			split = 0
+		end
+		
+		console.log(deedeeCount)
+		console.log(deedeeSplitsSub[split + 1])
 		
 		machineFlags1 = memory.read_u32_le(0x0325A4)
 		machineFlags2 = memory.read_u32_le(0x0325A8)
@@ -96,7 +106,7 @@ while true do
 		machineCount = 0
 		for i = 1,32 do
 			temp = machineFlags1 % 2
-			if temp >= 0 then
+			if temp >= 1 then
 				machineCount = machineCount + 1
 			end
 			machineFlags1 = machineFlags1 / 2
@@ -108,13 +118,14 @@ while true do
 			machineFlags2 = machineFlags2 / 2
 		end
 		
-		if (deedeeCount >= deedeeSplits[split + 1] and tools >= toolSplits[split + 1] and machineCount >= machineSplits[split + 1]) then
+		if ((useSubSplits and deedeeCount >= deedeeSplitsSub[split + 1] and tools >= toolSplitsSub[split + 1] and machineCount >= machineSplitsSub[split + 1]) or
+		((not useSubSplits) and deedeeCount >= deedeeSplits[split + 1] and tools >= toolSplits[split + 1] and machineCount >= machineSplits[split + 1])) then
 			send_time()
 			split = split + 1
 			pipe_handle:write("split\r\n")
 			pipe_handle:flush()
 		end
-	elseif (pausevar ~= 0x9F and mainmenucheck) then
+	elseif (not (pausevar == 0x9F or (loading ~= 65535 and useSubSplits)) and mainmenucheck) then
 		mainmenucheck = false
 	end
 	
